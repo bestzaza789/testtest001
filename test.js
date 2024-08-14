eaeb0528c881b235b4c7cbad93c94e57522c0504
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-08-14 16:43:21 +0700

    test best

--- test.js
+++ test.js
@@ -3,3 +3,14 @@ const express = require("express");
 const app = express();
 
 // Middleware to parse JSON request bodies
+
+app.use(express.json());
+
+// Middleware to log request and response details
+
+app.use((req, res, next) => {
+  console.log(`Request method: ${req.method}`);
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Request body: ${JSON.stringify(req.body)}`);
+  next();
+});
